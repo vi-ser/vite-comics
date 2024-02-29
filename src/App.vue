bfd2acf25d6014c704d08ec084e35d66cb884c19
vi_ser <vinceulisse@gmail.com>
2024-02-29 16:27:14 +0100

    hover pt.1

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import AppContent from './components/AppContent.vue';
 import AppNav from './components/AppNav.vue';
 import AppIconSection from './components/AppIconSection.vue'
+import AppFooter from './components/AppFooter.vue';
 
 export default {
   components: {
     AppNav,
     AppContent,
     AppIconSection,
+    AppFooter,
   }
 }
 </script>
@@ -16,6 +18,7 @@ export default {
   <AppNav></AppNav>
   <AppContent></AppContent>
   <AppIconSection></AppIconSection>
+  <AppFooter></AppFooter>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,136 @@
+<script>
+export default {
+    name: 'AppFooter',
+    data() {
+        return {
+            links: [
+                {
+                    title: "DC COMICS",
+                    list: [
+                        'Characters',
+                        'Comics',
+                        'Movies',
+                        'TV',
+                        'Games',
+                        'Videos',
+                        'News'
+                    ]
+                },
+                {
+                    title: "SHOP",
+                    list: [
+                        'Shop DC',
+                        'Shop Collectibles',
+                    ]
+                },
+                {
+                    title: "DC",
+                    list: [
+                        'Terms Of Use',
+                        'Privacy policy (New)',
+                        'Ad Choices',
+                        'Advertising',
+                        'Jobs',
+                        'Subscriptions',
+                        'Talent Workshops',
+                        'CPSC Certificates',
+                        'Ratings',
+                        'Shop Help',
+                        'Contact Us',
+                    ]
+                },
+                {
+                    title: "SITES",
+                    list: [
+                        'DC',
+                        'MAD Magazine',
+                        'DC Kids',
+                        'DC Universe',
+                        'DC Power Visa',
+                    ]
+                },
+
+            ]
+
+        }
+    },
+}
+
+</script>
+
+<template>
+    <footer>
+        <div class="links">
+            <div v-for="linkList in links" class="links-list">
+                <h3>{{ linkList.title }}</h3>
+                <ul>
+                    <li v-for="currentLink in linkList.list">
+                        {{ currentLink }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <img src="/img/dc-logo-bg.png" alt="">
+    </footer>
+</template>
+
+<style lang="scss">
+@use '../styles/variables' as *;
+
+footer {
+    display: flex;
+    justify-content: space-between;
+
+    height: 370px;
+    padding: 40px $containerPadding;
+    background-image: url('/img/footer-bg.jpg');
+    background-size: cover;
+
+    overflow: hidden;
+
+
+    .links {
+
+        display: flex;
+        flex-direction: column;
+        column-gap: 32px;
+        flex-wrap: wrap;
+
+        .links-list {
+
+            z-index: 1;
+
+            h3 {
+                font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+
+                margin-bottom: 12px;
+            }
+
+            ul {
+
+                padding: 0;
+                margin-bottom: 24px;
+
+                li {
+                    list-style-type: none;
+                    color: #959595;
+                    font-size: 14px;
+
+                    &:hover {
+                        color: white;
+                        cursor: pointer;
+                    }
+                }
+
+
+            }
+
+        }
+    }
+
+    img {
+        transform: scale(1.5);
+    }
+
+}
+</style>
