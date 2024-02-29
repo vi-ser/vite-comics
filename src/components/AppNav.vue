40ca0c3777817d6e88c07c8e94102bd267366607
vi_ser <vinceulisse@gmail.com>
2024-02-29 14:41:52 +0100

    aggiunta navbar

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -13,7 +13,78 @@ export default {
 </script>
 
 <template>
-    Navbar
+    <nav>
+        <div id="logo-container">
+            <img src="/img/dc-logo.png" alt="">
+        </div>
+        <ul>
+            <li v-for="currentLink, index in links" :class="index == activeLinkIndex ? 'active' : ''">
+                {{ currentLink }}
+            </li>
+        </ul>
+    </nav>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use '../styles/variables' as *;
+
+nav {
+    display: flex;
+    justify-content: space-between;
+
+    height: 118px;
+    align-items: center;
+
+    background-color: white;
+
+    padding: 0 320px;
+
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+
+
+
+    #logo-container {
+
+        display: flex;
+        align-items: center;
+
+        img {
+            height: 80px;
+        }
+    }
+
+    ul {
+        display: flex;
+        gap: 32px;
+        color: $secondaryColorLight;
+
+        li {
+            list-style-type: none;
+            text-transform: uppercase;
+            font-size: 14px;
+            font-weight: 700;
+
+            &.active {
+                color: $primaryColor;
+                position: relative;
+            }
+
+            &.active::after {
+                content: "";
+                width: 100%;
+                height: 5px;
+                background-color: $primaryColor;
+                position: absolute;
+                top: 64.4px;
+                left: 0;
+            }
+
+            &:hover {
+                cursor: pointer;
+            }
+        }
+    }
+
+
+}
+</style>
