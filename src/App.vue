a8977b811cedd14c3bdd3458d939e7d112965c0c
vi_ser <vinceulisse@gmail.com>
2024-02-29 13:55:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppNav from './components/AppNav.vue';
+
+export default {
+  components: {
+    AppNav,
+  }
+}
+</script>
+
+<template>
+  <AppNav></AppNav>
+</template>
+
+<style lang="scss">
+@use './styles/general' as *;
+</style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppNav',
+
+    data() {
+        return {
+            links: ["characters", "comics", "movies", "tv", "games", "collectibles", "videos", "fans", "news", "shop"],
+
+            activeLinkIndex: 1,
+        }
+    }
+}
+</script>
+
+<template>
+    Navbar
+</template>
+
+<style lang="scss"></style>
